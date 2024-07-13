18a704f2d4d21fef51527452f891445b82f343e1
kusa lachisa <kusalechisac@gmail.com>
2024-07-13 14:19:46 +0300

    yes

--- server/client/src/pages/profile/ProfileLogin.js
+++ server/client/src/pages/profile/ProfileLogin.js
@@ -4,10 +4,12 @@ import { useFormik } from "formik";
 import { usernameValidate } from "../../helper/validate";
 import { useAuthStore } from "../../store/store";
 import "bootstrap/dist/css/bootstrap.css"; // Import Bootstrap CSS
+import { useState } from "react";
 
 export default function ProfileLogin() {
   const navigate = useNavigate();
   const setUsername = useAuthStore((state) => state.setUsername);
+  const [loading, setLoading] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -17,14 +19,16 @@ export default function ProfileLogin() {
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async (values) => {
+      setLoading(true);
       setUsername(values.username);
+      setLoading(false);
       navigate("/profileupdate");
     },
   });
 
   return (
     <section
-      className=" bg-image"
+      className="bg-image"
       style={{
         backgroundColor: "rgb(255, 255, 255)",
         paddingTop: "33px",
@@ -46,9 +50,9 @@ export default function ProfileLogin() {
           height: "400px",
         }}
       >
-        <Toaster position="top-center" reverseOrder={false}></Toaster>
-        <div className="d-flex justify-content-center align-items-center">
-          <div className="glass py-2">
+        <Toaster position="top-center" reverseOrder={false} />
+        <div className="d-flex justify-content-center align-items-center h-100">
+          <div className="glass py-2 w-100">
             <div className="text-center">
               <br />
               <br />
@@ -58,13 +62,15 @@ export default function ProfileLogin() {
               <div className="d-flex flex-column align-items-center gap-3">
                 <input
                   {...formik.getFieldProps("username")}
-                  className="w-20"
+                  className="w-50 px-2"
                   type="text"
                   placeholder="username"
                 />
-                <button className="btn btn-success" type="submit">
-                  Login
-                </button>
+                {loading ? null : (
+                  <button className="btn btn-success" type="submit">
+                    Login
+                  </button>
+                )}
               </div>
             </form>
           </div>
