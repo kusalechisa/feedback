b0e03abce51f0bd7c62a238fee350ee42a2a32a6
kusa lachisa <kusalechisac@gmail.com>
2024-10-26 10:06:46 +0300

    Email updated

--- server/controllers/mailer.js
+++ server/controllers/mailer.js
@@ -5,7 +5,7 @@ import ENV from "../config.js";
 let nodeConfig = {
   host: "smtp.gmail.com",
   port: 587,
-  secure: false,
+  secure: false, // use STARTTLS
   auth: {
     user: ENV.EMAIL,
     pass: ENV.PASSWORD,
@@ -17,42 +17,45 @@ let transporter = nodemailer.createTransport(nodeConfig);
 let MailGenerator = new Mailgen({
   theme: "default",
   product: {
-    name: "Complaint and Anti-Corruption Adminstartors",
-    link: "http://mint.gov.et/?lang=en",
+    name: "Feedback and Anti-Corruption Administrators",
+    link: "https://kusalechisa.netlify.app",
   },
 });
 
 export const registerMail = async (req, res) => {
   const { username, userEmail, text, subject } = req.body;
 
-  // Create the body of the email using Mailgen
+  // Create the email body using Mailgen
   var email = {
     body: {
       name: username,
       intro:
         text ||
-        `Dear ${username},\n\nThank you for signing up for our feedback system Admin. We're excited to have you on board!`,
-      outro: "\n\nBest regards,\nThe Complaint and Anticorruption System Admin",
+        `Dear ${username},\n\nThank you for signing up for our feedback system. We're excited to have you on board!`,
+      outro: "\n\nBest regards,\nThe Feedback and Anti-Corruption System Admin",
     },
   };
 
-  // Generate the HTML email body using Mailgen
+  // Generate the HTML email body
   var emailBody = MailGenerator.generate(email);
 
   let message = {
     from: ENV.EMAIL,
     to: userEmail,
-    subject: subject || "You Signedup Successfully",
+    subject: subject || "You Signed Up Successfully",
     html: emailBody,
   };
 
   // Send the email
-  transporter
-    .sendMail(message)
-    .then(() => {
-      return res
-        .status(200)
-        .send({ msg: "You should receive an email from us." });
-    })
-    .catch((error) => res.status(500).send({ error }));
+  try {
+    await transporter.sendMail(message);
+    return res
+      .status(200)
+      .send({ msg: "You should receive an email from us." });
+  } catch (error) {
+    console.error("Email sending error:", error); // Log the error for troubleshooting
+    return res.status(500).send({
+      error: "There was an error sending the email. Please try again later.",
+    });
+  }
 };
